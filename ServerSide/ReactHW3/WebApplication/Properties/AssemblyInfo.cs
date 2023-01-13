7219f0880f36a0ab98e78dd890c88081d4e52940
DanielFigman <118261654+DanielFigman@users.noreply.github.com>
2023-01-13 18:54:51 +0200

    update

--- ServerSide/ReactHW3/WebApplication/Properties/AssemblyInfo.cs
+++ ServerSide/ReactHW3/WebApplication/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8646f1f2-21a6-49a8-bdcd-b19d21f74ba0")]
+[assembly: Guid("684eaf14-6694-4582-81ac-eaefde447b8d")]
 
 // Version information for an assembly consists of the following four values:
 //
